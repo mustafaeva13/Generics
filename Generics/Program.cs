c385e80584fa604ea1258a5bee503512fdf4f161
efsane <126017552+mustafaeva13@users.noreply.github.com>
2023-04-11 13:47:49 +0400

    New Commit

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -5,12 +5,12 @@ namespace Generics
 {
     public class Program
     {
-        static void Main(string[] args) 
-        
-        {
-            
+        static void Main(string[] args)
 
-        
+        {
+
+
+
             //Player player = new Player()
             //{
             //    Health = 100
@@ -25,18 +25,85 @@ namespace Generics
 
 
 
-        //    List<int>GenericList = new List<int>();
+            //List<int> GenericList = new List<int>();
 
-        //    GenericList.Add(30);
-        //    GenericList.Add(2);
-        //    GenericList.Add(3);
-        //        GenericList.Add(4);
+            //GenericList.Add(30);
+            //GenericList.Add(50);
+            //GenericList.Add(100);
+            //GenericList.Add(120);
+
+
+            //foreach(int i in GenericList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //}
+
+
+
+        }
+
+
+        #region   ///Task1
+
+
+        //class GameCollection<T>
+        //{
+        //    private List<T> gameCollection;
+
+        //    private List<T> GetGameCollection()
+        //    {
+        //        return gameCollection;
+        //    }
+
+        //    private void SetGameCollection(List<T> value)
+        //    {
+        //        gameCollection = value;
+        //    }
+
+        //    public void Add( T gameCollection)
+        //    {
+        //        GetGameCollection().Add(gameCollection);
+        //    }
         //}
+        #endregion
 
-       
 
-        
+        #region Task2
+        interface ICharacter<T>
+        {
+            void Attack(T attack);
+            void TakeDamage(T damage);
+            void Health(T health);
+        }
+
+
+        class Character : ICharacter<T>
+        {
+            public void Attack()
+            {
+                Console.WriteLine("Your attack");
+            }
+
+            public void Attack(T attack)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Health(T health)
+            {
+                Console.WriteLine("Your Health");
+            }
+
+            public void TakeDamage(T damage)
+            {
+                Console.WriteLine("Your takeDamege");
+            }
+        }
+        #endregion
+
+
+
+
     }
-
-    //Verilən əşyanın oyunçunun inventarında olub-olmadığını müəyyən etmək üçün istifadə edilə bilən IsItemInInverntory adlı generic metod hazırlayın.
 }
